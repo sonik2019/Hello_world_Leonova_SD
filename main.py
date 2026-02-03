31d94418b91109c7a6263aad60bb06e3accd81de
sonik2019 <sonik2019.com@gmail.com>
2026-02-03 17:14:21 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+# Моя первая программа
+user_name = "Студент"
+number_a = 15
+number_b = 7
+
+print(f"Привет, {user_name}!")
+print(f"Сумма чисел {number_a} и {number_b} равна {number_a + number_b}")
